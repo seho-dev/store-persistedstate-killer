2fe70fcdca1174c9ecf3f973373d65ebec720d5d
seho <1018715564@qq.com>
2022-07-01 13:29:03 +0800

    🐞 fix: 预定义的存储驱动更改存取格式
    为了解决null, undefined等js类型的问题

--- src/storage/driver.ts
+++ src/storage/driver.ts
@@ -8,8 +8,8 @@ import { DefineStorageDriver } from '../../typings/storage'
 export const defineStorageDriver: DefineStorageDriver = (name) => {
   const storage = name === 'localStorage' ? localStorage : sessionStorage
   return {
-    setItem: (key: string, value: string) => storage.setItem(key, value),
-    getItem: (key: string) => storage.getItem(key),
+    setItem: (key: string, value: string) => storage.setItem(key, JSON.stringify({ value })),
+    getItem: (key: string) => JSON.parse(storage.getItem(key) as string).value,
     removeItem: (key: string) => storage.removeItem(key),
     key: (index: number) => storage.key(index),
     length: storage.length
